0c6839680310267a408f9c5b3182933c37108706
Shogo Okajima <mmfujishiro+zwei@gmail.com>
2021-02-02 23:13:00 +0900

    WIP: image_folder機能を追加
    image_folder機能を追加したが正しく動いているのか不明

--- project/dataset_loader.py
+++ project/dataset_loader.py
@@ -1,8 +1,9 @@
+from typing import Sequence
+
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Subset
 from torchvision import transforms as tf
-from torchvision.datasets import MNIST
-from typing import Sequence
+from torchvision.datasets import MNIST, ImageFolder
 
 
 class DatasetLoader:
@@ -27,6 +28,34 @@ class DatasetLoader:
         if indices is not None:
             dataset = Subset(dataset, indices)
         return dataset
+
+    @staticmethod
+    def image_folder(dirctory: str, transform: tf.transforms.Compose = None):
+        """自作画像クラス分類データセット生成
+
+        Args:
+            dirctory (str): ディレクトリパス
+            transform (tf.transforms.Compose, optional): 画像変換チェーン. Defaults to None.
+
+        Returns:
+            dataset: 自作画像クラス分類データセット
+
+        Example:
+            /path/to/
+            ├─ class1: クラス1の画像があるディレクトリ
+            │   ├─ a.jpg
+            │   ├─ b.jpg
+            │   └─ c.jpg
+            ├─ class2: クラス2の画像があるディレクトリ
+            │   ├─ a.jpg
+            │   ├─ b.jpg
+            │   └─ c.jpg
+            └─ class3: クラス3の画像があるディレクトリ
+                 ├─ a.jpg
+                 ├─ b.jpg
+                 └─ c.jpg
+        """
+        return ImageFolder(dirctory, transform=transform)
 
     @staticmethod
     def make_loader(dataset, batch_size: int, shuffle: bool = False, num_workers: int = 0, pin_memory: bool = True):
--- project/train.py
+++ project/train.py
@@ -1,8 +1,7 @@
-import re
-
 import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
+from cnn_finetune import make_model
 
 from dataset_loader import DatasetLoader
 from models.cnnnet import CNNNet
@@ -13,25 +12,37 @@ class PLModule(pl.LightningModule):
 
     def __init__(self):
         super().__init__()
-        self.net = CNNNet()
+        # self.net = CNNNet()
+        self.net = make_model('resnet18', num_classes=2, pretrained=True)
+        self.accuracy = pl.metrics.Accuracy()
 
     def forward(self, x):
         return self.net(x)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
-        y_hat = self(x)
+        # y_hat = self(x)
+        y_hat = self.net(x)
         loss = F.cross_entropy(y_hat, y)
+        self.log('train_loss', loss)
+        # log step metric
+        self.log('train_acc_step', self.accuracy(y_hat, y))
         return loss
 
     def configure_optimizers(self):
-        return torch.optim.SGD(self.parameters(), lr=0.001, momentum=0.9)
+        # return torch.optim.SGD(self.parameters(), lr=0.001, momentum=0.9)
+        return torch.optim.Adam(self.parameters())
+
+    def training_epoch_end(self, outs):
+        # log epoch metric
+        self.log('train_acc_epoch', self.accuracy.compute())
 
 
 def main():
     transform = Transforms.compose()
-    train_dataset = DatasetLoader.mnist(transform=transform, indices=list(range(0, 1000)))
-    train_loader = DatasetLoader.make_loader(train_dataset, batch_size=100, num_workers=4)
+    # train_dataset = DatasetLoader.mnist(transform=transform)
+    train_dataset = DatasetLoader.image_folder("tmp/train", transform=transform)
+    train_loader = DatasetLoader.make_loader(train_dataset, batch_size=10, num_workers=4)
 
     # init model
     model = PLModule()
